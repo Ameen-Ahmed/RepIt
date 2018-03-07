45c8779edaec75518f517b0cd9510468a3c57deb
Ameen Ahmed <ameenahmed39@gmail.com>
2018-03-07 15:35:07 -0500

    Include auth_functions.php

--- private/initialize.php
+++ private/initialize.php
@@ -5,6 +5,8 @@ require_once('functions.php');
 require_once('database_functions.php');
 require_once('query_functions.php');
 require_once('validation_functions.php');
+require_once('auth_functions.php');
+
 
 $db = db_connect();
 $errors = [];
