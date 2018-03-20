5464038f88dca9d0b797541456cea07ca58c8e46
Ameen Ahmed <ameenahmed39@gmail.com>
2018-03-19 20:03:57 -0400

    Add new utility functions

--- private/functions.php
+++ private/functions.php
@@ -3,6 +3,14 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 
+function url_for($script_path) {
+  // add the leading '/' if not present
+  if($script_path[0] != '/') {
+    $script_path = "/" . $script_path;
+  }
+  return WWW_ROOT . $script_path;
+}
+
 function u($string="") {
     return urlencode($string);
 }
@@ -15,6 +23,10 @@ function h($string="") {
     return htmlspecialchars($string);
 }
 
+function redirect_to($location) {
+  header("Location: " . $location);
+  exit();
+}
 
 function is_post_request() {
     return $_SERVER['REQUEST_METHOD'] == 'POST';
